0b9bd1721439b0e1a1f6b9db6840613afbc07523
VIka Sahaidak <sahaidakvika12@gmail.com>
2025-11-02 22:44:42 -0800

    final finished code with right pre and post comments

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,7 +1,9 @@
 // Name: Viktoriia Sahaidak
 // Class: CS143 Java 2
-// Assignment: SudokuBoard
-// This program will read data from text file and load it into the class, preparing data for the sudoku game
+// Assignment: SudokuBoard #3
+//HW Core topics: recursion, backtracking
+// This program will read data from text file, load it into the class, 
+// and solve the puzzle
 
 import java.io.*;
 import java.util.*;
@@ -12,7 +14,7 @@ public class SudokuBoard {
    //pre: readable text file with 9 lines and 9 char each
    //post: fills the 9x9 char array with digits or '.' from the file
    public SudokuBoard(String fileName) throws FileNotFoundException {
-      board = new char[9][9]; //creates the board 9x9
+      board = new char[9][9]; 
       
       Scanner input = new Scanner(new File(fileName));
       
@@ -27,9 +29,9 @@ public class SudokuBoard {
       input.close();
    } 
    
-   //pre: array lists filled with data and initialized 
-   //post: returns a formatted string with 9 lines
-   public String toString() {
+   //pre: none 
+   //post: returns a formatted string showing the current board
+      public String toString() {
       String result = "";
       for (int row = 0; row < 9; row++) {
          if (row % 3 == 0) {
@@ -49,7 +51,7 @@ public class SudokuBoard {
    }
    
    //pre:  isValid passed
-   //post: returns true if passes the requirements, returns false otherwise 
+   //post: returns true if passes the requirements 
    public boolean isSolved() {
      if (!isValid()) return false;
      int[] count = new int[10];
@@ -68,14 +70,14 @@ public class SudokuBoard {
    }
    
    //pre:  4 other methods exist and return something
-   //post: passes the validation if everything returns true, false otherwise
-   public boolean isValid() {
+   //post: passes the validation if each method returns true
+      public boolean isValid() {
       return dataValid() && rowValid() && colValid() && boxValid();
   }
   
   //pre:  board[r][c] exists and has data in it
-  //post: returns true if data is 1-9 or '.', returns false if contains something else
-   private boolean dataValid() {
+  //post: returns true if data is 1-9 or '.'
+     private boolean dataValid() {
       Set<Character> allowed = new HashSet<>(Arrays.asList(
       '.','1','2','3','4','5','6','7','8','9'));
       
@@ -88,7 +90,7 @@ public class SudokuBoard {
    } 
    
   //pre:  board[r][c] exists and has data in it
-  //post: returns true if row has no duplicates, false otherwise
+  //post: returns true if row has no duplicates
    private boolean rowValid() {
       for (int r = 0; r < 9; r++) {
          if (!checkUnits(r, 0, 0, 1)) return false;
@@ -97,7 +99,7 @@ public class SudokuBoard {
    } 
    
    //pre:  board[r][c] exists and has data in it
-   //post: returns true if colom has no duplicates, false otherwise
+   //post: returns true if colom has no duplicates
    private boolean colValid() {
       for (int c = 0; c < 9; c++) {
          if (!checkUnits(0, c, 1, 0)) return false;
@@ -106,7 +108,7 @@ public class SudokuBoard {
    } 
    
    //pre:  board[r][c] exists and has data in it
-   //returns true if mini square has no duplicates, false otherwise
+   //returns true if mini square has no duplicates
    private boolean boxValid() {
       for (int sr = 0; sr < 9; sr += 3) {
          for (int sc = 0; sc < 9; sc += 3) {
@@ -128,8 +130,8 @@ public class SudokuBoard {
    
    //helpers
    //pre:  board[r][c] exists and has data in it
-   //post: returns false if has duplicates, true if doesn't
-   public boolean checkUnits(int r0, int c0, int dr, int dc) {
+   //post: returns true if doesn't have duplicates
+      public boolean checkUnits(int r0, int c0, int dr, int dc) {
       boolean[] seen = new boolean[10];            
       int r = r0;
       int c = c0;
@@ -148,6 +150,8 @@ public class SudokuBoard {
       return true;
   }
   
+  //pre:  board is loaded
+  //post: returns true if solution was found
    public boolean solve() {
       if (!isValid()) { 
          return false;
@@ -158,6 +162,9 @@ public class SudokuBoard {
       return solveHelper();
    }
    
+   //pre:  board is valid
+   //post: fills empty cells by backtracking; 
+   //      returns true if solved, false if no solution
    public boolean solveHelper() {
       int row = -1;
       int col = -1;
@@ -184,3 +191,41 @@ public class SudokuBoard {
       return false;
    }
 }
+
+/*
+ ----jGRASP exec: java -ea SudokuSolverEngine
+ Initial board
+ +-------+-------+-------+
+ | . 3 4 | 6 7 8 | 9 1 2 |
+ | . 7 2 | 1 9 5 | 3 4 8 |
+ | 1 9 8 | 3 4 2 | 5 6 7 |
+ +-------+-------+-------+
+ | . . 9 | . 6 1 | 4 2 3 |
+ | . 2 6 | 8 5 3 | 7 9 1 |
+ | . 1 3 | 9 2 4 | . 5 6 |
+ +-------+-------+-------+
+ | . 6 1 | 5 3 7 | 2 8 4 |
+ | . 8 . | 4 1 9 | 6 3 5 |
+ | 3 4 5 | . 8 6 | 1 7 9 |
+ +-------+-------+-------+
+ 
+ 
+ Solving board...SOLVED in 0,006 seconds.
+ 
+ +-------+-------+-------+
+ | 5 3 4 | 6 7 8 | 9 1 2 |
+ | 6 7 2 | 1 9 5 | 3 4 8 |
+ | 1 9 8 | 3 4 2 | 5 6 7 |
+ +-------+-------+-------+
+ | 8 5 9 | 7 6 1 | 4 2 3 |
+ | 4 2 6 | 8 5 3 | 7 9 1 |
+ | 7 1 3 | 9 2 4 | 8 5 6 |
+ +-------+-------+-------+
+ | 9 6 1 | 5 3 7 | 2 8 4 |
+ | 2 8 7 | 4 1 9 | 6 3 5 |
+ | 3 4 5 | 2 8 6 | 1 7 9 |
+ +-------+-------+-------+
+ 
+ 
+  ----jGRASP: Operation complete.
+  */
